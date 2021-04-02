5dad1d032b4a04665edbc50f8c0314d016de45bd
Andrew Kraynak <abk89@miami.edu>
2021-04-02 11:10:26 -0400

    Fixed issue on sample pages

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import '../constants/page_titles.dart';
 import '../widgets/app_scaffold.dart';
 
-class GalleryPage extends StatelessWidget {
-  const GalleryPage({Key key}) : super(key: key);
+class AccountPage extends StatelessWidget {
+  const AccountPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/pages/locations_page.dart
+++ lib/pages/locations_page.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import '../constants/page_titles.dart';
 import '../widgets/app_scaffold.dart';
 
-class GalleryPage extends StatelessWidget {
-  const GalleryPage({Key key}) : super(key: key);
+class LocationsPage extends StatelessWidget {
+  const LocationsPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
