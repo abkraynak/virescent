651bdb617253240ea8f33043e5eb1a77b146975e
Andrew Kraynak <abk89@miami.edu>
2021-04-02 10:54:40 -0400

    Added constants

---
+++ lib/constants/page_titles.dart
@@ -0,0 +1,7 @@
+class PageTitles {
+  static const String home = 'Home';
+  static const String locations = 'Locations';
+  static const String camera = 'Camera';
+  static const String account = 'Account';
+  static const String settings = 'Settings';
+}
---
+++ lib/constants/route_names.dart
@@ -0,0 +1,7 @@
+class RouteNames {
+  static const String home = '/';
+  static const String locations = '/locations';
+  static const String camera = '/camera';
+  static const String account = '/account';
+  static const String settings = '/settings';
+}
