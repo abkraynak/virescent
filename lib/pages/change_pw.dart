f5ff0ec3009bbdc064c483b66d74a92eada25e0f
Andrew Kraynak <abk89@miami.edu>
2021-04-03 19:47:12 -0400

    Added text fields to change password page

--- lib/pages/change_pw.dart
+++ lib/pages/change_pw.dart
@@ -1,14 +1,75 @@
 import 'package:flutter/material.dart';
 
-class ChangePasswordPage extends StatelessWidget {
+import '../constants/colors.dart';
+
+class ChangePasswordPage extends StatefulWidget {
+  @override
+  _ChangePasswordPageState createState() => _ChangePasswordPageState();
+}
+
+class _ChangePasswordPageState extends State<ChangePasswordPage> {
   final String title = "Change Password";
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(this.title),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(this.title),
+        ),
+        body: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+                child: Column(children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: "  Current password",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: "  New password",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: "  New password, again",
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: RaisedButton(
+                  color: themeColor,
+                  onPressed: () {},
+                  child: Text('Change Password',
+                      style: TextStyle(color: Colors.white, fontSize: 18)),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                ),
+              )
+            ]))));
   }
 }
