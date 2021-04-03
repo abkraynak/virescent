4d776e6ad48e1549e86cdaf865ea60b7907d3820
Andrew Kraynak <abk89@miami.edu>
2021-04-03 15:25:49 -0400

    Added sample options to the settings page

--- lib/widgets/settings_options.dart
+++ lib/widgets/settings_options.dart
@@ -3,22 +3,27 @@ import 'package:flutter/material.dart';
 class SettingsOptions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Scaffold(
-                backgroundColor: Colors.white,
-                body: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Center(
-                        child: Text('This is the NEW settings page'),
-                      ),
-                      Center(
-                        child: Text('Second line of text'),
-                      ),
-                    ]))));
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          title: Text('Option 1'),
+          subtitle: Text('Description'),
+          onTap: () {},
+        ),
+        SwitchListTile(
+          value: false,
+          title: Text('Option 2'),
+          onChanged: (value) {},
+        ),
+        SwitchListTile(
+          value: true,
+          title: Text('Option 3'),
+          subtitle: Text('Description'),
+          onChanged: (value) {},
+        ),
+        CheckboxListTile(
+            value: true, title: Text('Option 4'), onChanged: (value) {}),
+      ],
+    );
   }
 }
