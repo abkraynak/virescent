901891771739bb1113a78047ed5a7312b2dfc4cc
Andrew Kraynak <abk89@miami.edu>
2021-04-03 14:49:59 -0400

    Moved page code to a widget

--- lib/pages/locations.dart
+++ lib/pages/locations.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 
 import '../constants/page_titles.dart';
 import '../widgets/app_scaffold.dart';
+import '../widgets/locations_map.dart';
 
 class LocationsPage extends StatelessWidget {
   const LocationsPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const AppScaffold(
+    return AppScaffold(
       pageTitle: PageTitles.locations,
-      body: Center(
-        child: Text('This is the locations page'),
-      ),
+      body: LocationsMap(),
     );
   }
 }
---
+++ lib/widgets/locations_map.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LocationsMap extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('This is the NEW locations page'),
+      ),
+    );
+  }
+}
