4b49ae4e30b95829b624fdd80a97ff7a02a55eec
Andrew Kraynak <abk89@miami.edu>
2021-04-04 19:50:37 -0400

    Added Google Maps API to the Locations page

--- lib/widgets/locations_map.dart
+++ lib/widgets/locations_map.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class LocationsMap extends StatelessWidget {
+class LocationsMap extends StatefulWidget {
+  @override
+  _LocationsMapState createState() => _LocationsMapState();
+}
+
+class _LocationsMapState extends State<LocationsMap> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(25.761681, -80.191788);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('This is the Locations page'),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
       ),
+
     );
   }
 }
