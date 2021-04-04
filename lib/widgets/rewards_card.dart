df0afd1fd9382fb12686778a2d923cc320e328cd
Andrew Kraynak <abk89@miami.edu>
2021-04-04 17:33:01 -0400

    Added sample information to rewards page

---
+++ lib/widgets/rewards_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class RewardsCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const ListTile(
+            title: Text('Reward'),
+            subtitle: Text('This is the description of the reward'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TextButton(
+                child: const Text('REDEEM'),
+                onPressed: () {},
+              ),
+              const SizedBox(width: 8),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/rewards_list.dart
+++ lib/widgets/rewards_list.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/rewards_card.dart';
+
 class RewardsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('This is the rewards page'),
-      ),
-    );
+    return ListView(children: <Widget>[
+      RewardsCard(),
+      RewardsCard(),
+      RewardsCard(),
+      RewardsCard(),
+      RewardsCard(),
+      RewardsCard(),
+      RewardsCard(),
+    ]);
   }
 }
