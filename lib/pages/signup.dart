00475e11cf4604f3d626cc148591e8f39c6bcc45
Andrew Kraynak <abk89@miami.edu>
2021-04-09 14:15:10 -0400

    Updated signup screen padding to use constants

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
-import '../constants/page_titles.dart';
 import '../constants/buttons.dart';
+import '../constants/page_titles.dart';
+import '../constants/positioning.dart';
 import 'email_login.dart';
 import 'email_signup.dart';
 
@@ -19,18 +20,21 @@ class SignUpPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.all(10.0),
+                  padding: EdgeInsets.symmetric(
+                      vertical: Paddings.ver, horizontal: Paddings.hor),
                   child: Text('Welcome to Virescent',
                       style:
                           TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
                 ),
                 Padding(
-                  padding: EdgeInsets.all(10.0),
+                  padding: EdgeInsets.symmetric(
+                      vertical: Paddings.ver, horizontal: Paddings.hor),
                   child: Text('Let\'s get started!',
                       style: TextStyle(fontSize: 20)),
                 ),
                 Padding(
-                    padding: EdgeInsets.all(10.0),
+                    padding: EdgeInsets.symmetric(
+                        vertical: Paddings.ver, horizontal: Paddings.hor),
                     child: SignInButton(
                       Buttons.Email,
                       text: 'Sign up with Email',
@@ -43,21 +47,24 @@ class SignUpPage extends StatelessWidget {
                       },
                     )),
                 Padding(
-                    padding: EdgeInsets.all(10.0),
+                    padding: EdgeInsets.symmetric(
+                        vertical: Paddings.ver, horizontal: Paddings.hor),
                     child: SignInButton(
                       Buttons.Google,
                       text: 'Sign up with Google',
                       onPressed: () {},
                     )),
                 Padding(
-                    padding: EdgeInsets.all(10.0),
+                    padding: EdgeInsets.symmetric(
+                        vertical: Paddings.ver, horizontal: Paddings.hor),
                     child: SignInButton(
                       Buttons.Facebook,
                       text: 'Sign up with Facebook',
                       onPressed: () {},
                     )),
                 Padding(
-                    padding: EdgeInsets.all(10.0),
+                    padding: EdgeInsets.symmetric(
+                        vertical: Paddings.ver, horizontal: Paddings.hor),
                     child: TextButton(
                         child: Text('Log In Using Email',
                             style: TextButtons.mainTextStyle,),
