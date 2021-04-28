664f031b5f0359bb47341171d2761c4670ff7daf
Andrew Kraynak <abk89@miami.edu>
2021-04-28 16:11:53 -0400

    Added Firebase DB for user accounts to account page

--- lib/widgets/account_info.dart
+++ lib/widgets/account_info.dart
@@ -1,9 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 import '../constants/colors.dart';
 import '../constants/route_names.dart';
 
-class AccountInfo extends StatelessWidget {
+class AccountInfo extends StatefulWidget {
+  AccountInfo({ this.app });
+  final FirebaseApp app;
+
+  @override
+  _AccountInfoState createState() => _AccountInfoState();
+}
+
+class _AccountInfoState extends State<AccountInfo> {
+  final referenceDb = FirebaseDatabase.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
