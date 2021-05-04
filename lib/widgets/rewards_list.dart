37fc4bd78a9289aee257a0968dac57f2c7c8db2c
Andrew Kraynak <abk89@miami.edu>
2021-05-04 00:02:09 -0400

    Code cleanup

--- lib/widgets/rewards_list.dart
+++ lib/widgets/rewards_list.dart
@@ -1,12 +1,9 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 import '../constants/colors.dart';
 import '../constants/route_names.dart';
-import '../models/rewards.dart';
-//import '../widgets/rewards_card.dart';
 
 class RewardListItem {
   String id;
@@ -24,14 +21,6 @@ class RewardListItem {
     this.description = obj['description'];
     this.redeemed = obj['redeemed'];
   }
-  /*
-  String get _id => id;
-  String get _company => company;
-  String get _cost => cost;
-  String get _description => description;
-  String get _redeemed => redeemed;
-
-   */
 
   RewardListItem.fromSnapshot(DataSnapshot snapshot) {
     id = snapshot.key;
@@ -41,49 +30,6 @@ class RewardListItem {
     redeemed = snapshot.value['redeemed'];
   }
 }
-/*
-class RewardList {
-  List<RewardListItem> rewardList;
-  RewardList({this.rewardList});
-
-  factory RewardList.fromJSON(Map<dynamic, dynamic> json) {
-    return RewardList(rewardList: parserewards(json));
-  }
-
-  static List<RewardListItem> parserewards(rewardJSON) {
-    var rList = rewardJSON['browseRewards'] as List;
-    List<RewardListItem> rewardList =
-        rList.map((data) => RewardListItem.fromJson(data)).toList();
-    return rewardList;
-  }
-}
-
-final dbRef = FirebaseDatabase.instance.reference();
-
-class DBServ {
-  static Future<List<RewardDB>> getRewards() async {
-    Query rewardSnapshot = await FirebaseDatabase.instance
-        .reference()
-        .child("rewards")
-        .orderByKey();
-
-    print(rewardSnapshot); // to debug and see if data is returned
-    print('here/n');
-
-    List<RewardDB> rewards = [];
-/*
-    Map<dynamic, dynamic> values = needsSnapshot.path.value;
-    values.forEach((key, values) {
-      rewards.add(RewardDB.fromSnapshot(values));
-    });
-
- */
-
-    return rewards;
-  }
-}
-
- */
 
 class RewardsList extends StatefulWidget {
   @override
@@ -104,15 +50,6 @@ class _RewardsListState extends State<RewardsList> {
     _onRewardAddedSubscription =
         rewardsReference.onChildAdded.listen(_onRewardAdded);
   }
-/*
-  _setupRewards() async {
-    List<RewardDB> rewards = await DBServ.getRewards();
-    setState(() {
-      _rewards = rewards;
-    });
-  }
-
- */
 
   void _onRewardAdded(Event event) {
     setState(() {
